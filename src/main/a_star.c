d92fb39f172892b9b1b90cdca70c15b719543c92
Joe Doherty <thriron9@gmail.com>
2017-06-07 19:28:01 -0500

    Initial commit to set up file structure and add first files.

---
+++ src/main/a_star.c
@@ -0,0 +1,35 @@
+
+typdef struct _location {
+    int x;
+    int y;
+} Location;
+
+Location * nav_stack;
+int nav_stack_size, nav_stack_current_size;
+
+add(Location pos){
+    int i,j;
+    int distance;
+    distance = distance(pos);
+    for(i=0; i<nav_stack_current_size; i++){
+        if(distance < distance(nav_stack[i])){
+            //copy down
+            if(nav_stack_size == nav_stack_current_size){
+                nav_stack_size *= 2;
+                nav_stack_current_size++;
+                //realloc;
+            }
+            for(j=i; j<nav_stack_current_size; j++)
+            //insert
+            nav_stack[i] = pos;
+            return;
+        }
+    }
+    //add to end
+    if(nav_stack_size == nav_stack_current_size){
+        nav_stack_size *= 2;
+        //realloc
+    }
+    nav_stack_size++;
+    nav_stack[nav_stack_size] = pos;
+}
---
+++ src/main/main.c
@@ -0,0 +1,55 @@
+
+#define MAP_SIZE 16
+
+/*          0001
+ *         ______
+ *        |      |
+ *  1000  |[x][y]| 0010
+ *        |______|
+ *
+ *          0100
+ */ 
+#define UP 0x01
+#define DOWN 0x04
+#define LEFT 0x08
+#define RIGHT 0x02
+uint8_t map[MAP_SIZE][MAP_SIZE];
+
+int init(){
+    memset(map, 0, sizeof(map));
+    //initialize walls
+    for(int i=0; i<MAP_SIZE; i++){
+        map[i][0] |= UP;
+        map[i][MAP_SIZE] |= DOWN;
+        map[0][i] |= LEFT;
+        map[MAP_SIZE-1][i] |= RIGHT;
+    }
+}
+
+void set_walls(uint8_t wall, uint8_t x, uint8_t y){//TODO: mark walls as visited
+    map[x][y]=wall;
+    if((wall & UP)!=0){
+        if((y+1) < MAP_SIZE){
+            map[x][y+1]|= DOWN;
+        }
+    }
+    if((wall & DOWN)!=0){
+        if((y-1) >= 0){
+            map[x][y-1]|= UP;
+        }
+    }
+    if((wall & RIGHT)!=0){
+        if((x+1) < MAP_SIZE){
+            map[x+1][y]|= LEFT;
+        }
+    }
+    if((wall & LEFT)!=0){
+        if((x-1) >= 0){
+            map[x-1][y]|= RIGHT;
+        }
+   }
+}
+
+void main(){
+    init();
+}
